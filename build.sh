3777a96874247b61b8653f29bff24bb610bec725
LeTabouret <simstemarie@gmail.com>
2024-05-26 09:21:45 -0400

    Fix build failling.

--- build.sh
+++ build.sh
@@ -12,8 +12,13 @@ INCLUDED_PACKAGES=($(jq -r "[(.all.include | (select(.\"$PACKAGE_LIST\" != null)
                              | sort | unique[]" /tmp/packages.json))
 
 # Build list of all packages requested for override
-CONFLICTED_PACKAGES=($(jq -r "[(.all.conflictedpackages | (select(.\"$PACKAGE_LIST\" != null).\"$PACKAGE_LIST\")[]), \
-                             (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".conflictedpackages | (select(.\"$PACKAGE_LIST\" != null).\"$PACKAGE_LIST\")[])] \
+CONFLICTED_PACKAGES=($(jq -r "[(.all.conflictedpackagesupdates | (select(.\"$PACKAGE_LIST\" != null).\"$PACKAGE_LIST\")[]), \
+                             (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".conflictedpackagesupdates | (select(.\"$PACKAGE_LIST\" != null).\"$PACKAGE_LIST\")[])] \
+                             | sort | unique[]" /tmp/packages.json))
+
+ # Build list of all packages requested for override in base
+CONFLICTED_PACKAGES_BASE=($(jq -r "[(.all.conflictedpackagesbase | (select(.\"$PACKAGE_LIST\" != null).\"$PACKAGE_LIST\")[]), \
+                             (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".conflictedpackagesbase | (select(.\"$PACKAGE_LIST\" != null).\"$PACKAGE_LIST\")[])] \
                              | sort | unique[]" /tmp/packages.json))
 
 # Build list of all packages requested for exclusion
@@ -22,18 +27,24 @@ EXCLUDED_PACKAGES=($(jq -r "[(.all.exclude | (select(.\"$PACKAGE_LIST\" != null)
                              | sort | unique[]" /tmp/packages.json))
 
 
+# Install packages that are in base install and may generate conflicts
+if [[ "${#CONFLICTED_PACKAGES_BASE[@]}" -gt 0 ]]; then
+    rpm-ostree install \
+        ${CONFLICTED_PACKAGES_BASE[@]}
+fi
+
+# Install conflicted packages to resolve dependencies
+if [[ "${#CONFLICTED_PACKAGES[@]}" -gt 0 ]]; then
+    rpm-ostree override replace \
+    --experimental \
+    --from repo=updates \
+        ${CONFLICTED_PACKAGES[@]} \
+        || true
+fi
+
 # Ensure exclusion list only contains packages already present on image
 if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
     EXCLUDED_PACKAGES=($(rpm -qa --queryformat='%{NAME} ' ${EXCLUDED_PACKAGES[@]}))
-fi
-
-# Remove potentially conflicting packages if they are installed
-if [[ "${#CONFLICTED_PACKAGES[@]}" -gt 0 ]]; then
-    for pkg in "${CONFLICTED_PACKAGES[@]}"; do
-        if rpm -q $pkg &>/dev/null; then
-            rpm-ostree override remove $pkg
-        fi
-    done
 fi
 
 # Simple case to install where no packages need excluding
@@ -49,11 +60,6 @@ elif [[ "${#INCLUDED_PACKAGES[@]}" -gt 0 && "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]];
 
 else
     echo "No packages to install."
-fi
-
-# Install conflicted packages to resolve dependencies
-if [[ "${#CONFLICTED_PACKAGES[@]}" -gt 0 ]]; then
-    rpm-ostree install ${CONFLICTED_PACKAGES[@]}
 fi
 
 # Check if any excluded packages are still present
